fa65bef33e0289b5ea180e38d5d50511513374bd
Libertas <1adamsvoboda@gmail.com>
2022-10-31 20:15:24 +0100

    changed Hello World to benchmark test, added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kasiopea 2022
+
+Řešení programátorské soutěže Kasiopea pro rok 2022.
+
+Toto repo obsahuje jen úlohy, které jsem úspěšně vyřešil, či ty, které jsem začal řešit.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::time::{Instant};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting...");
+    let start = Instant::now();
+
+    const NUMBER: i64 = 1_000_000_000;
+    let mut i = 0;
+
+    while i != NUMBER {
+        i+=1;
+    }
+
+    let duration = start.elapsed();
+
+    println!("Time elapsed is: {:?}", duration);
 }
