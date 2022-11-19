139265b5cc8c3500c240d8695d02d1c2dc15389d
Libertas <1adamsvoboda@gmail.com>
2022-11-19 08:53:47 +0100

    dart instead of rust

---
+++ bin/kasiopea2022.dart
@@ -0,0 +1,5 @@
+import 'package:kasiopea2022/kasiopea2022.dart' as kasiopea2022;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kasiopea2022.calculate()}!');
+}
---
+++ test/kasiopea2022_test.dart
@@ -0,0 +1,8 @@
+import 'package:kasiopea2022/kasiopea2022.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
